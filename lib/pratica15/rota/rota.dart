c9e2f60ab11dc0154fd902bdd1f8d6c2ae5c21e6
SergioHMagalhaes <76484748+SergioHMagalhaes@users.noreply.github.com>
2021-10-14 21:30:34 -0300

    commit

--- lib/pratica15/rota/rota.dart
+++ lib/pratica15/rota/rota.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: PrimeiraRota(),
-    ));
-
 class PrimeiraRota extends StatefulWidget {
   PrimeiraRotaState createState() {
     return PrimeiraRotaState();
