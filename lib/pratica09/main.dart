38dded5eaf2bde3d013ee4f38dbd00af42e16e1f
Sérgio <ssergioh3@gmail.com>
2021-10-28 13:56:08 -0300

    commit

---
+++ lib/pratica09/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  int numeroVezes = 0;
+  void cliqueDoBotao() {
+    numeroVezes = numeroVezes + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Página Inicial"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "Número de vezes em que o botão foi pressionado: $numeroVezes.\nEsse número é ${numeroVezes % 2 == 0? "par" : "ímpar"}"),
+                
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(cliqueDoBotao);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
